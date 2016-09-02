0455ba36f6719e5854fa800dde1bfc57850b226a
KangBingkui <178974662@qq.com>
2016-09-02 16:28:08 +0800

    update

--- src/script/ToastTip.js
+++ src/script/ToastTip.js
@@ -2,8 +2,9 @@
  * Created by kang on 16-9-2.
  */
 import React from 'react';
+import '../style/tip.css';
 
-let Tip = React.createClass({
+let ToastTip = React.createClass({
     propTypes:{
         type:React.PropTypes.string,
         timeout:React.PropTypes.number
@@ -26,16 +27,19 @@ let Tip = React.createClass({
             });
         },this.props.timeout);
     },
+    _getWidth(){
+        let _width = this.refs._toast.style.width;
+        console.log(_width);
+    },
     _returnClassName(){
-        let _className = "mc-toast-";
+        let _className = "ui-toast ui-toast-";
         _className += this.props.type;
         return _className;
     },
     render(){
         //this._timeOut();
-        return <div className="mc-toast">
-            <div className={this._returnClassName()}>{this.props.children}</div>
-        </div>;
+        this._getWidth();
+        return this.state.visible ? (<div ref="_toast" className={this._returnClassName()} style={{marginLeft:""}}>{this.props.children}</div>) : null;
     }
 });
-module.exports = Tip;
+module.exports = ToastTip;
