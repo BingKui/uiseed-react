7d11402242646d18ee8e24575f0ed2d9cc1d6373
KangBingkui <178974662@qq.com>
2016-09-04 23:22:59 +0800

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Tip from './src/script/Tip'
+import Tip from './src/script/ToastTip'
 
 let btnHandleClick = function(){
     console.log("这个是传入的函数！");
--- src/script/ToastTip.js
+++ src/script/ToastTip.js
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import '../style/tip.css';
+import $ from "jquery";
 
 let ToastTip = React.createClass({
     propTypes:{
@@ -27,9 +28,15 @@ let ToastTip = React.createClass({
             });
         },this.props.timeout);
     },
+    componentDidMount(){
+        this._getWidth();
+    },
     _getWidth(){
-        let _width = this.refs._toast.style.width;
-        console.log(_width);
+        let _toast = this.refs._toast;
+        let _left = - ($(_toast).width()) / 2;
+        $(_toast).css({
+            marginLeft: _left
+        });
     },
     _returnClassName(){
         let _className = "ui-toast ui-toast-";
@@ -38,8 +45,9 @@ let ToastTip = React.createClass({
     },
     render(){
         //this._timeOut();
-        this._getWidth();
+
         return this.state.visible ? (<div ref="_toast" className={this._returnClassName()} style={{marginLeft:""}}>{this.props.children}</div>) : null;
+
     }
 });
 module.exports = ToastTip;
